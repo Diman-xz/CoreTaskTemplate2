4b0bf33b2934b2f0a4bf728a85be89b5611016a6
Diman-xz <87063734+Diman-xz@users.noreply.github.com>
2021-09-20 12:26:27 +0300

    1.1.3

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,19 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Alex", "Sidorov", (byte) 32);
+        userService.saveUser("Ivan", "Ivanov", (byte) 54);
+        userService.saveUser("Petr", "Petrov", (byte) 24);
+        userService.saveUser("Vasay", "Pupkin", (byte) 19);
+        System.out.println(userService.getAllUsers());
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,27 @@
 package jm.task.core.jdbc.util;
 
+
+import com.mysql.fabric.jdbc.FabricMySQLDriver;
+
+import java.sql.*;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+
+    private static final String URL = "jdbc:mysql://localhost:3306/UsersTable?autoReconnect=true&useSSL=false";
+    private static final String USER = "root";
+    private static final String PASSWORD = "rootroot";
+
+    public Connection getConnection() {
+        Connection connection = null;
+        try {
+            Driver driver = new FabricMySQLDriver();
+            DriverManager.registerDriver(driver);
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            System.out.println("Соединение не установлено");
+            e.printStackTrace();
+        }
+        return connection;
+    }
 }
+
